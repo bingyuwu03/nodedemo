95dec76e22f36a2ae9ee7f213fa053572aafbf74
bingyuwu03 <147114508+bingyuwu03@users.noreply.github.com>
2024-04-26 21:04:35 -0400

    Update stock.js

--- stock.js
+++ stock.js
@@ -1,16 +1,17 @@
-var http = require('http');
-var url = require('url');
+const http = require("http");
+const url = require("url");
 const { MongoClient } = require('mongodb');
 
-const connstr = "your_connection_string_here";
+const connstr = "mongodb+srv://bingyuwu03:Qsrmmwubingyu123@cluster0.ncotu9a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const dbName = 'Stock';
 const collectionName = 'PublicCompanies';
 
-http.createServer(async function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    var path = url.parse(req.url, true).pathname;
+const server = http.createServer((req, res) => {
+    console.log("Success");
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    const path = url.parse(req.url, true).pathname;
     if (path == "/") {
-        var formHTML = `
+        const formHTML = `
             <form action="/process" method="get">
                 <input type='text' name='search' placeholder='Enter Ticker or Company Name'>
                 <br>
@@ -20,42 +21,53 @@ http.createServer(async function (req, res) {
                 <input type='submit' value='Submit'>
             </form>`;
         res.write(formHTML);
+        res.end();
     } else if (path == "/process") {
-        var query = url.parse(req.url, true).query;
-        var searchType = query.type;
-        var searchTerm = query.search;
-        
+        const query = url.parse(req.url, true).query;
+        const searchType = query.type;
+        const searchTerm = query.search;
+
         try {
-            const db = await MongoClient.connect(connstr);
-            const database = db.db(dbName);
-            const collection = database.collection(collectionName);
-            
-            var filter = {};
-            if (searchType === 'ticker') {
-                filter = { "ticker": searchTerm };
-            } else if (searchType === 'company') {
-                filter = { "name": searchTerm };
-            }
-            
-            const curs = collection.find(filter);
-            const companies = await curs.toArray();
-            
-            res.write("<h2>Search Results:</h2>");
-            companies.forEach(company => {
-                res.write(`<p>Name: ${company.name}, Ticker: ${company.ticker}, Price: ${company.price}</p>`);
+            const client = new MongoClient(connstr, { useNewUrlParser: true, useUnifiedTopology: true });
+            client.connect(async err => {
+                if (err) {
+                    console.error(err);
+                    res.write("An error occurred.");
+                    res.end();
+                    return;
+                }
+                const db = client.db(dbName);
+                const collection = db.collection(collectionName);
+
+                let filter = {};
+                if (searchType === 'ticker') {
+                    filter = { "ticker": searchTerm };
+                } else if (searchType === 'company') {
+                    filter = { "name": searchTerm };
+                }
+
+                const companies = await collection.find(filter).toArray();
+
+                res.write("<h2>Search Results:</h2>");
+                companies.forEach(company => {
+                    res.write(`<p>Name: ${company.name}, Ticker: ${company.ticker}, Price: ${company.price}</p>`);
+                });
+
+                console.log("Search Results:");
+                console.log(companies);
+
+                client.close();
+                res.end();
             });
-            
-            // Displaying data on the console
-            console.log("Search Results:");
-            console.log(companies);
-            
-            // Close the database connection
-            db.close();
-            
         } catch (error) {
             console.error(error);
             res.write("An error occurred.");
+            res.end();
         }
     }
-    res.end();
-}).listen(8080);
+});
+
+server.listen(8080, () => {
+    console.log('Server is running on http://localhost:8080');
+});
+
